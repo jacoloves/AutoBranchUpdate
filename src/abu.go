069b4f63f71ae344549dbace3dbb2a2919720552
jacoloves <5511068t@gmail.com>
2022-09-18 22:14:00 +0900

    git pull feature add

--- src/abu.go
+++ src/abu.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -20,12 +21,33 @@ func main() {
 	defer os.Chdir(prev)
 
 	gitBranch()
+	gitPullBranch(nil, TRAGET_REPO)
 }
 
 func gitBranch() {
 	output, err := exec.Command("git", "branch").CombinedOutput()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 	}
 	fmt.Printf("%s", output)
+}
+
+func gitPullBranch(in io.Reader, repoName string) {
+	if in == nil {
+		in = os.Stdin
+	}
+	fmt.Printf("git pull %s? >", repoName)
+
+	var inputValue string
+	fmt.Fscan(in, &inputValue)
+	switch inputValue {
+	case "y", "Y", "yes", "YES":
+		output, err := exec.Command("git", "pull", "--progress", "origin").CombinedOutput()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
+		fmt.Printf("%s", output)
+	default:
+		fmt.Printf("do not git pull %s\n", TRAGET_REPO)
+	}
 }
--- src/abu_test.go
+++ src/abu_test.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
 )
 
-func TestGitBranch(t *testing.T) {
-	tests := [...]string{
-		".",                        // current direcotry git branch exist
-		"/home",                    // home direcotry git branch not exist
-		"/home/tests/test_abu_dir", // test direcotry git branch exist but remote repository no exist
-	}
+var testdirs = []string{
+	".",                        // current direcotry git branch exist
+	"/home",                    // home direcotry git branch not exist
+	"/home/tests/test_abu_dir", // test direcotry git branch exist but remote repository no exist
+}
 
+var yesstrings = []string{
+	"y",
+	"Y",
+	"yes",
+	"YES",
+	"yas",
+	"YeS",
+	"yeS",
+	"Yes",
+	"year!",
+	" y ",
+	"y    ",
+	"      y",
+}
+
+func Test_gitBranch(t *testing.T) {
 	prev, err := filepath.Abs(".")
 	if err != nil {
 		os.Exit(1)
 	}
 	defer os.Chdir(prev)
 
-	for _, dir := range tests {
+	for _, dir := range testdirs {
 		target, err := filepath.Abs(dir)
 		if err != nil {
 			os.Exit(1)
@@ -28,5 +45,47 @@ func TestGitBranch(t *testing.T) {
 		os.Chdir(target)
 
 		gitBranch()
+	}
+}
+
+func Test_gitPullBranch(t *testing.T) {
+	prev, err := filepath.Abs(".")
+	if err != nil {
+		os.Exit(1)
+	}
+	defer os.Chdir(prev)
+
+	for _, dir := range testdirs {
+		target, err := filepath.Abs(dir)
+		if err != nil {
+			os.Exit(1)
+		}
+
+		os.Chdir(target)
+
+		in := bytes.NewBufferString("y")
+		gitPullBranch(in, "feature")
+	}
+
+	for _, dir := range testdirs {
+		target, err := filepath.Abs(dir)
+		if err != nil {
+			os.Exit(1)
+		}
+
+		os.Chdir(target)
+
+		in := bytes.NewBufferString("n")
+		gitPullBranch(in, "feature")
+	}
+
+	fmt.Println("--- input nil test ---")
+	gitPullBranch(nil, "feature")
+
+	fmt.Println("--- input yes strings test ---")
+	os.Chdir(prev)
+	for _, input := range yesstrings {
+		in := bytes.NewBufferString(input)
+		gitPullBranch(in, "feature")
 	}
 }
